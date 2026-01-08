201c92cf7b8cdda01c58ac947c039872a354cdab
Azone Projects <projects.azone@gmail.com>
2026-01-08 23:45:51 +0400

    fix: Revert AOS and Bootstrap to afterInteractive strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -136,8 +136,8 @@ export default function RootLayout({
 
         {/* Third Party Libs - Defer loading */}
         <Script src="/assets/vendor/purecounter/purecounter_vanilla.js" strategy="lazyOnload" />
-        <Script src="/assets/vendor/aos/aos.js" strategy="lazyOnload" />
-        <Script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" strategy="lazyOnload" />
+        <Script src="/assets/vendor/aos/aos.js" strategy="afterInteractive" />
+        <Script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" strategy="afterInteractive" />
         <Script src="/assets/vendor/glightbox/js/glightbox.min.js" strategy="lazyOnload" />
         <Script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js" strategy="lazyOnload" />
         <Script src="/assets/vendor/swiper/swiper-bundle.min.js" strategy="lazyOnload" />
